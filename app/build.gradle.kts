da36716ad579d52fdd4062735fb5202414e06f6a
Norman Hooper <nhooper@dimagi.com>
2025-06-27 20:38:08 +0100

    Add tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,6 +86,7 @@ dependencies {
 
     // Testing
     testImplementation(libs.junit)
+    testImplementation(libs.mockk)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
